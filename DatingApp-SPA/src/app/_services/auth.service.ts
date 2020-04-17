abfb77cc23984d7ccc736c22155194d515f82d62
chrisjamaica <chriscargill2761@gmail.com>
2020-04-16 21:02:13 -0400

    added alertifyJs to alert users of errors, thid party packages for UI and token handling

--- DatingApp-SPA/src/app/_services/auth.service.ts
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { Token } from '../_model/token.model';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,8 @@ import { Token } from '../_model/token.model';
 export class AuthService {
 
   baseURL = 'http://localhost:5000/api/auth/';
+  jwtHelper = new JwtHelperService();
+  decodedToken: any;
 
   constructor(private http: HttpClient) { }
 
@@ -19,6 +22,7 @@ export class AuthService {
           const user = response;
           if (user) {
             localStorage.setItem('token', user.token);
+            this.decodedToken = this.jwtHelper.decodeToken(user.token);
           }
         })
       );
@@ -26,6 +30,11 @@ export class AuthService {
 
   register(model: any) {
     return this.http.post(`${this.baseURL}register`, model);
+  }
+
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    return !this.jwtHelper.isTokenExpired(token);
   }
 
 }
