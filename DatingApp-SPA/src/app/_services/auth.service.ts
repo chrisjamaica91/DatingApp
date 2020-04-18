fce997b7dacec1dd581739c887c98da59955844f
chrisjamaica <chriscargill2761@gmail.com>
2020-04-18 10:35:12 -0400

    added in photograph gallery on user details page

--- DatingApp-SPA/src/app/_services/auth.service.ts
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -1,15 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
-import { Token } from '../_model/token.model';
+import { Token } from '../_models/token.model';
 import { JwtHelperService } from '@auth0/angular-jwt';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  baseURL = 'http://localhost:5000/api/auth/';
+  baseURL = `${environment.apiUrl}auth/`;
   jwtHelper = new JwtHelperService();
   decodedToken: any;
 
