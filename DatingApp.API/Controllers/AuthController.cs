4052699f6ba80a9d0feefd14109d245969fec73d
chrisjamaica <chriscargill2761@gmail.com>
2020-04-26 09:38:49 -0400

    expanded registration form and added default main photo image

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -37,14 +37,13 @@ namespace DatingApp.API.Controllers
             if (await _repo.UserExists(user.Username))
                 return BadRequest("Username already exists");
 
-            var userToCreate = new User
-            {
-                Username = user.Username
-            };
+            var userToCreate = _mapper.Map<User>(user);
 
             var createdUser = await _repo.Register(userToCreate, user.Password);
 
-            return StatusCode(201);
+            var userToReturn = _mapper.Map<UserForDetailedDTO>(createdUser);
+
+            return CreatedAtRoute("GetUser", new {controller = "Users", id = createdUser.Id}, userToReturn);
         }
 
         [HttpPost("login")]
