2fd1e25071a73028768216afad302699d31dee28
chrisjamaica <chriscargill2761@gmail.com>
2020-04-30 12:54:28 -0400

    added in like functionality

--- DatingApp-SPA/src/app/_services/user.service.ts
+++ DatingApp-SPA/src/app/_services/user.service.ts
@@ -15,7 +15,7 @@ export class UserService {
 
   constructor(private http: HttpClient) { }
 
-  getUsers(page?, itemsPerPage?, userParams?): Observable<PaginatedResult<User[]>> {
+  getUsers(page?, itemsPerPage?, userParams?, likesParams?): Observable<PaginatedResult<User[]>> {
     const paginatedResult: PaginatedResult<User[]> = new PaginatedResult<User[]>();
 
     let params = new HttpParams();
@@ -30,7 +30,14 @@ export class UserService {
       params = params.append('maxAge', userParams.maxAge);
       params = params.append('gender', userParams.gender);
       params = params.append('orderBy', userParams.orderBy);
+    }
 
+    if (likesParams === 'Likers') {
+      params = params.append('likers', 'true');
+    }
+
+    if (likesParams === 'Likees') {
+      params = params.append('likees', 'true');
     }
     return this.http.get<User[]>(`${this.baseUrl}users`, { observe: 'response', params})
       .pipe(
@@ -58,5 +65,9 @@ export class UserService {
 
   deletePhoto(userId: number, photoId: number) {
     return this.http.delete(`${this.baseUrl}users/${userId}/photos/${photoId}`);
+  }
+
+  sendLike(id: number, recipientId: number) {
+    return this.http.post(`${this.baseUrl}users/${id}/like/${recipientId}`, {});
   }
 }
